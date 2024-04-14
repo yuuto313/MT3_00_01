f29df1e715896dcb30508e76dd959887df8c2d89
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-04-14 15:56:38 +0900

    MT3_00_01

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <Novice.h>
+#include <Vector3.h>
+#include <math.h>
 
 const char kWindowTitle[] = "LE2B_04_オザワ_ユウト";
 
-struct Vector3 {
-	float x, y, z;
-};
 //加算
 Vector3 Add(const Vector3& v1, const Vector3& v2) {
 	Vector3 result;
@@ -35,11 +34,15 @@ float Dot(const Vector3& v1, const Vector3& v2) {
 }
 //長さ
 float Length(const Vector3& v) {
-	return v.x * v.x + v.y * v.y + v.z * v.z;
+	return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
 }
 //正規化
 Vector3 Normalize(const Vector3& v) {
-
+	Vector3 result;
+	result.x = v.x / Length(v);
+	result.y = v.y / Length(v);
+	result.z = v.z / Length(v);
+	return Vector3(result);
 }
 
 static const int kColumnWidth = 60;
@@ -48,7 +51,7 @@ void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) 
 	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
 	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
 	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
-	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "&s", label);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
 }
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -98,7 +101,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		VectorScreenPrintf(0, kRowHeight, resultSubtract, " : Subtract");
 		VectorScreenPrintf(0, kRowHeight *2, resultMultliply, " : Multriply");
 		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f : Dot", resultDot);
-		Novice::ScreenPrintf(0, kRowHeight * 4, "%..02f : Length", resultLength);
+		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f : Length", resultLength);
 		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, " : Normalize");
 
 		///
